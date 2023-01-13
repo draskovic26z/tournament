1361fb346719180f4f6c0cb055f41c8c886a9f9a
Nikola Drašković <draskovic26z@gmail.com>
2023-01-13 15:24:34 +0100

    some trait ideas

--- programs/tournament/src/constants/specials.rs
+++ programs/tournament/src/constants/specials.rs
@@ -3,33 +3,33 @@ use anchor_lang::{AnchorDeserialize, AnchorSerialize};
 
 #[derive(Debug, AnchorSerialize, AnchorDeserialize, Clone)]
 pub enum Special {
-    //Background
-    Grassland,
-    Battlefield,
-    Castle, // Opponent doesnt attack first round
-    Hell,   // On first death, revive with 30hp
+    //Background -> Match wide buffs
+    Grassland,   // Attack twice in the first round
+    Battlefield, // Execute enemy when two rounds in a row he is below 30%
+    Castle,      // Opponent doesnt attack first and last round
+    Hell,        // Revive with 30% hp on first death
 
-    //Helmet
-    Iron,
-    Bronze,
-    Gold,
-    Jade,
+    //Helmet ->  Compensation self buffs
+    Iron,   // If your ATK is lower than opponents DEF, gain more ATK
+    Bronze, // If your AP is lower than opponents AR, gain more ATK
+    Gold,   // If your AR is lower than opponents AP, gain more DEF
+    Jade,   // If your DEF is lower than opponents ATK, gain more DEF
 
-    //Body
+    //Body ->
     Plate,
     Leather,
     Emerald,
-    PLACEHOLDER2,
+    PLACEHOLDER,
 
-    //Hand
-    Protected,
-    Fiery,
-    Bloody,
-    Bare,
+    //Hand -> Passive on each "round"
+    Protected, // Every time you take dmg, opponent takes 30% of that dmg too
+    Fiery,     // Burn 5% of opponents max health on each attack
+    Bloody,    // Lifesteal: Heal 30% of all dmg you deal
+    Bare,      // Gain AP based on your DEF/AR
 
-    //Weapon
-    Halberd,
-    Sword,
-    Shield,
-    SpikedShield,
+    //Weapon -> Snowballng self buffs
+    Halberd,      // If your ATK is higher than opponents ATK, gain more atk
+    Sword,        // If your AP is higher than opponents AP, gain more ap
+    Shield,       // If your DEF is higher than opponents DEF, gain more DEF
+    SpikedShield, // If your AR is higher opponents AR, gain more AR
 }
